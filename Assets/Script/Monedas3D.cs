dfc292cae22361b1ad4236ef70e236d00a2a4f76
WhisperCheng <ivandominguez@sos-sistemas.com>
2023-11-14 14:40:18 +0000

    Teoria clase
    No a habido nada nuevo en el código pero se va a hacer una explicación en clase

--- Assets/Script/Monedas3D.cs
+++ Assets/Script/Monedas3D.cs
@@ -6,6 +6,7 @@ public class Monedas3D : MonoBehaviour
 {
 
     private ParticleSystem particulas;
+    bool pillado;
 
 
     // Start is called before the first frame update
@@ -13,19 +14,24 @@ public class Monedas3D : MonoBehaviour
     {
         particulas = GetComponent<ParticleSystem>();
         particulas.Stop();
+        pillado = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (pillado == true)
+        {
+            Destroy(gameObject, particulas.duration);
+            
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Personaje")
         {
-            Destroy(gameObject, particulas.duration);
+            pillado = true;
             particulas.Play();
         }
     }
